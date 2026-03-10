2c704b73f7d56599caffc00272a2aa3722de80be
Amir Nurmukhambetov <thereug6@gmail.com>
2026-03-10 18:14:05 +0500

    added orders and scans models (#6)

--- schema.sql
+++ schema.sql
@@ -5,3 +5,35 @@ CREATE TABLE IF NOT EXISTS users (
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS orders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sender_id INTEGER NOT NULL,
+    receiver_id INTEGER NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    meta TEXT,
+    comment TEXT,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_sender
+    FOREIGN KEY (sender_id) REFERENCES users(id)
+    CONSTRAINT fk_receiver
+    FOREIGN KEY (receiver_id) REFERENCES users(id)
+    CONSTRAINT sender_receiver
+    CHECK (sender_id <> receiver_id)
+);
+
+CREATE TABLE IF NOT EXISTS scans (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    order_id INTEGER NOT NULL,
+    courier_id INTEGER NOT NULL,
+    photo BLOB,
+    condition VARCHAR(255) NOT NULL,
+    longitude DECIMAL,
+    latitude DECIMAL,
+    comment TEXT,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_order
+    FOREIGN KEY (order_id) REFERENCES orders(id)
+    CONSTRAINT fk_courier
+    FOREIGN KEY (courier_id) REFERENCES users(id)
+);
+
